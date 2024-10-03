7912d8cd65a63b391b6849b8e7dfd0e88594d69a
NguyenVanThang2004 <154830941+NguyenVanThang2004@users.noreply.github.com>
2024-10-03 08:35:21 +0700

    Bài 11. Chỉnh sửa sản phẩm

--- Areas/Admin/Controllers/HomeAdminController.cs
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -29,6 +29,8 @@ namespace WebBanVali.Areas.Admin.Controllers
             return View(lst);
         }
 
+        // them moi san pham
+
         [Route("ThemSanPhamMoi")]
         [HttpGet]
 
@@ -57,5 +59,45 @@ namespace WebBanVali.Areas.Admin.Controllers
             return View(sanPham);
 
         }
+
+
+        // sua san pham
+
+
+
+        [Route("SuaSanPham")]
+        [HttpGet]
+
+        public IActionResult SuaSanPham(string maSanPham)
+        {
+
+            ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
+            ViewBag.MaHangSx = new SelectList(db.THangSxes.ToList(), "MaHangSx", "HangSx");
+            ViewBag.MaNuocSx = new SelectList(db.TQuocGia.ToList(), "MaNuoc", "TenNuoc");
+            ViewBag.MaLoai = new SelectList(db.TLoaiSps.ToList(), "MaLoai", "Loai");
+            ViewBag.MaDt = new SelectList(db.TLoaiDts.ToList(), "MaDt", "TenLoai");
+
+            var sanPham = db.TDanhMucSps.Find(maSanPham);
+            return View(sanPham);
+        }
+
+        [Route("SuaSanPham")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult SuaSanPham(TDanhMucSp sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sanPham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("DanhMucSanPham","HomeAdmin");
+            }
+            return View(sanPham);
+
+        }
+
+
+
     }
 }
